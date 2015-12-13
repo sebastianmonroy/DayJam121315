d9cfc4cd71fddd968dcf90862ac8247a19350197
Sebastian Monroy <sebastian.monroy@gmail.com>
2015-12-13 17:54:12 -0500

    basic movement

--- Assets/_Scripts/JumperController.cs
+++ Assets/_Scripts/JumperController.cs
@@ -6,6 +6,7 @@ public class JumperController : InputController
 {
 	public Jumper jumper;
 	public float resizeFactor;
+	public float movementFactor;
 
 	public override void Idle() 
 	{
@@ -23,8 +24,12 @@ public class JumperController : InputController
 		jumper.LerpRadius(1.0f + (expandInput - contractInput) * resizeFactor);
 	}
 
-	public void MovementControls(Vector2 LeftStick)
+	public void MovementControls(Vector2 movementInput)
 	{
-
+		float lateralMovement = movementInput.x;
+		if (Mathf.Abs(lateralMovement) > 0.05f) 
+		{
+			this.transform.position += new Vector3(lateralMovement * movementFactor, 0f);
+		}
 	}
 }
